2b14f93af8458b9bb46dcf1ab84e9fa96be670dd
ayush547 <ayushsinghs@yahoo.in>
2020-08-01 21:10:08 +0530

    Added register page

--- Agri/app/src/main/java/com/example/agri/RegisterActivity.java
+++ Agri/app/src/main/java/com/example/agri/RegisterActivity.java
@@ -1,14 +1,58 @@
 package com.example.agri;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.agri.pojos.Farmers;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class RegisterActivity extends AppCompatActivity {
+    Button contBtn;
+    Farmers farmer;
+    FirebaseAuth mAuth;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        farmer = new Farmers();
+        farmer.setMobNo(getIntent().getStringExtra("MobNo"));
+
+        contBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO:Take inputs and verify if correct then go to main page
+
+                //after verification and fixing created Farmer class object
+                CollectionReference dbUsers = db.collection("Users");
+                dbUsers.document(mAuth.getUid()).set(farmer).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        //here is where we go into the app
+                        startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Sign Up Failed, Try Again " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 }
--- Agri/app/src/main/java/com/example/agri/pojos/Farmers.java
+++ Agri/app/src/main/java/com/example/agri/pojos/Farmers.java
@@ -5,4 +5,56 @@ import java.util.List;
 public class Farmers {
     List<Crops> myCrops;
     String Id;
+    String AadhaarNo, MobNo, Email;
+
+    public Farmers() {
+    }
+
+    public Farmers(List<Crops> myCrops, String id, String aadhaarNo, String mobNo, String email) {
+        this.myCrops = myCrops;
+        Id = id;
+        AadhaarNo = aadhaarNo;
+        MobNo = mobNo;
+        Email = email;
+    }
+
+    public String getAadhaarNo() {
+        return AadhaarNo;
+    }
+
+    public void setAadhaarNo(String aadhaarNo) {
+        AadhaarNo = aadhaarNo;
+    }
+
+    public String getMobNo() {
+        return MobNo;
+    }
+
+    public void setMobNo(String mobNo) {
+        MobNo = mobNo;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public List<Crops> getMyCrops() {
+        return myCrops;
+    }
+
+    public void setMyCrops(List<Crops> myCrops) {
+        this.myCrops = myCrops;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
 }
