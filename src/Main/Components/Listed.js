030b3b54acb8bbc9a275fbc576ffe90fa429e554
gouthamcm <goutham.898@gmail.com>
2020-08-03 08:43:35 +0530

    Available crops are now being displayed

--- src/Main/Components/Listed.js
+++ src/Main/Components/Listed.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import fire, { db } from '../../Config/fire.js';
 import GridList from '@material-ui/core/GridList';
-import { GridListTile, ListSubheader } from '@material-ui/core';
+import { GridListTile, ListSubheader, Grid, Paper, ButtonBase } from '@material-ui/core';
 import { GridListTileBar } from '@material-ui/core';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+// import Typography from 'material-ui/styles/typography';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+// import Card from '@material-ui/core/Card';
+
+
 class Listed extends Component {
 
     constructor(props) {
@@ -24,7 +35,7 @@ class Listed extends Component {
             isError: false
         }
     }
-    componentDidMount() {
+    componentWillMount() {
         db.collection('Agri').doc('Crops').get().then(doc => {
             const d = doc.data();
             console.log(d.cropsList)
@@ -41,7 +52,7 @@ class Listed extends Component {
                 isLoading: false,
                 isError: false
             });
-        }).catch(function (error) {
+        }).catch((error) => {
             console.log("Error writing document: ", error);
             this.setState({
                 isLoading: false,
@@ -51,27 +62,107 @@ class Listed extends Component {
     }
 
     render() {
-        const { classes} = this.props;
+        const { classes } = this.props;
         return (
-            <div>
-                <GridList cellHeight={180} >
-                    <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-                        <ListSubheader component="div">Crops availability</ListSubheader>
-                    </GridListTile>
-                    {this.state.data.map((tile) => (
-                        <GridListTile key={tile.imageURL}>
-                            <img src={tile.imageURL} alt={tile.cropName} />
-                            <GridListTileBar
-                                title={tile.cropName}
-                                subtitle={<span>by: {tile.price}</span>}
+            <div style={styles.root}>
+                <br></br><br></br><br></br>
+                <br></br><br></br>
+                {this.state.data.map((crop) => (
+                    <div>
+                        {/* <Card style={styles.root}>
+                            <CardActionArea>
+                                <CardMedia
+                                    style={styles.media}
+                                    image={crop.imageURL}
+                                    title="Contemplative Reptile"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        {crop.cropName}
+                                    </Typography>
+                                    
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    Buy Crop
+                                </Button>
                                 
-                            />
-                        </GridListTile>
-                    ))}
-                </GridList>
+                            </CardActions>
+                        </Card> */}
+                        <Paper style={styles.paper}>
+                            <Grid container spacing={2}>
+                                <Grid item>
+                                    <ButtonBase style={styles.image}>
+                                        <img style={styles.img} alt="complex" src={crop.imageURL} />
+                                    </ButtonBase>
+                                </Grid>
+                                <Grid item xs={12} sm container>
+                                    <Grid item xs container direction="column" spacing={2}>
+                                        <Grid item xs>
+                                            <Typography gutterBottom variant="h5">
+                                                {crop.cropName}
+                                            </Typography>
+                                            <Typography variant="body2" gutterBottom>
+                                                Expected Date: 
+                                            </Typography>
+                                            <Typography variant="body2" color="subtitle1">
+                                                Organic: {crop.organic ? 'Yes' : 'No'}
+                                            </Typography>
+
+                                            <Typography variant="body2" color="subtitle1">
+                                                Total Quantity: {crop.totalQuantity}
+                                            </Typography>
+                                            <Typography variant="body2" color="subtitle1">
+                                                Remaining Quantity: {crop.remainingQuantity}
+                                            </Typography>
+
+                                        </Grid>
+                                        <Grid item>
+                                            {crop.remainingQuantity? <Button size="small" color="primary">
+                                                Buy Crop
+                                            </Button>: <Button size="small" color="primary" disabled>
+                                            Buy Crop
+                                        </Button> }
+                                            
+                                        </Grid>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant="subtitle1">{"Rs. " + crop.price + ' /quintal'}</Typography>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                        <br></br>
+                    </div>
+                ))}
+
             </div>
 
         )
     }
+}
+
+
+
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: '10px',
+        margin: 'auto',
+        maxWidth: 500,
+    },
+    image: {
+        width: '',
+        height: '100%',
+    },
+    img: {
+        margin: 'auto',
+        display: 'block',
+        maxWidth: '100%',
+        maxHeight: '100%',
+    },
 }
 export default Listed;
